879c6d7c3c9e65269c7a00d80bd388051834aa40
Ashfak Uzzaman <mdashfak0508@gmail.com>
2024-09-16 09:56:51 +0600

    V-1

--- app/src/main/java/com/example/tictactoe/GameActivity.java
+++ app/src/main/java/com/example/tictactoe/GameActivity.java
@@ -13,23 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class GameActivity extends AppCompatActivity {
 
-    boolean gameEnd=false;
-    int numberOfTurns=0;
+    boolean gameEnd = false;
+    int numberOfTurns = 0;
 
+    // Player representation and Game state :
     // 0 = O
     // 1 = X
     //-1 = Null
+
     int activePlayer = 0;
 
     int[] gameState = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
 
     final int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
-                                 {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
-                                 {0, 4, 8}, {2, 4, 6}};
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
 
     public void playerTap(View view) {
 
-        if(gameEnd){
+        if (gameEnd) {
             return;
         }
 
@@ -47,7 +49,7 @@ public class GameActivity extends AppCompatActivity {
             if (activePlayer == 0) {
                 img.setImageResource(R.drawable.img_o);
 
-                TextView statusBar=findViewById(R.id.textViewStatus);
+                TextView statusBar = findViewById(R.id.textViewStatus);
                 statusBar.setText("X's Turn");
 
                 activePlayer = 1;
@@ -55,7 +57,7 @@ public class GameActivity extends AppCompatActivity {
             } else {
                 img.setImageResource(R.drawable.img_x);
 
-                TextView statusBar=findViewById(R.id.textViewStatus);
+                TextView statusBar = findViewById(R.id.textViewStatus);
                 statusBar.setText("O's Turn");
 
                 activePlayer = 0;
@@ -67,28 +69,28 @@ public class GameActivity extends AppCompatActivity {
 
         // Checking if any one won or not
 
-        for(int[] winPosition:winPositions){
+        for (int[] winPosition : winPositions) {
 
-            if(gameState[winPosition[0]]==gameState[winPosition[1]] &&
-                    gameState[winPosition[0]]==gameState[winPosition[2]] &&
-                    gameState[winPosition[0]]!=-1){
+            if (gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                    gameState[winPosition[0]] == gameState[winPosition[2]] &&
+                    gameState[winPosition[0]] != -1) {
 
-                gameEnd=true;
+                gameEnd = true;
 
-                if(gameState[winPosition[0]]==0){
-                    TextView statusBar=findViewById(R.id.textViewStatus);
+                if (gameState[winPosition[0]] == 0) {
+                    TextView statusBar = findViewById(R.id.textViewStatus);
                     statusBar.setText("'O' has Won\nTap Here to Play Again\n ↺");
-                }
-                else{
-                    TextView statusBar=findViewById(R.id.textViewStatus);
+                } else {
+                    TextView statusBar = findViewById(R.id.textViewStatus);
                     statusBar.setText("'X' has Won\nTap Here to Play Again\n↺");
                 }
             }
         }
 
-        if(numberOfTurns==9){
-            gameEnd=true;
-            TextView statusBar=findViewById(R.id.textViewStatus);
+        // Checking if all the turns end or not
+        if (numberOfTurns == 9) {
+            gameEnd = true;
+            TextView statusBar = findViewById(R.id.textViewStatus);
             statusBar.setText("Tied\nTap Here to Play Again\n↺");
 
         }
@@ -97,32 +99,34 @@ public class GameActivity extends AppCompatActivity {
     }
 
 
-    public void gameReset(View view){
+    public void gameReset(View view) {
 
-        if(!gameEnd) {
+        if (!gameEnd) {
             return;
         }
 
-        activePlayer=0;
-        numberOfTurns=0;
-        gameEnd=false;
+        activePlayer = 0;
+        numberOfTurns = 0;
+        gameEnd = false;
 
-        for(int i=0;i<9;++i){
-            gameState[i]=-1;
+        // Reset gameState
+        for (int i = 0; i < 9; ++i) {
+            gameState[i] = -1;
         }
 
-        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+        // Clearing the images of Grid. (Setting the resource ID to 0  might be intended to clear the image displayed by the ImageView)
+        ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
 
         TextView status = findViewById(R.id.textViewStatus);
-        status.setText("O's Turn - Tap to play");
+        status.setText("O's Turn \n Tap to play");
 
     }
 
@@ -130,7 +134,7 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
 
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
